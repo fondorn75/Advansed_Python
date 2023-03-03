8e334880b7fb5811ddee0b342d1fd1c009a25c74
dmitrii dobrin <zatoichi.cadzicu@gmail.com>
2023-03-03 12:53:50 +0300

    hex

--- HW/hexValue.py
+++ HW/hexValue.py
@@ -3,4 +3,32 @@
 
 number = int(input("Введите число: "))
 
+result = ""
+temp = 0
+
 print(hex(number))
+
+while number != 0:
+    temp = number % 16
+    number = number // 16
+    match temp:
+        case 0: result += "0"
+        case 1: result += "1"
+        case 2: result += "2"
+        case 3: result += "3"
+        case 4: result += "4"
+        case 5: result += "5"
+        case 6: result += "6"
+        case 7: result += "7"
+        case 8: result += "8"
+        case 9: result += "9"
+        case 10: result += "a"
+        case 11: result += "b"
+        case 12: result += "c"
+        case 13: result += "d"
+        case 14: result += "e"
+        case 15: result += "f"
+
+result = ''.join(reversed(result))
+print(f"0x{result}")
+
--- HW/userFractions.py
+++ HW/userFractions.py
@@ -1,5 +1,6 @@
 # Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
 # Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
+import fractions
 
 string1 = input("Введите первую дробь: ")
 string2 = input("Введите вторую дробь: ")
@@ -7,3 +8,7 @@ string2 = input("Введите вторую дробь: ")
 summFractions = 12 + 12
 multiplicationFractions = 12 * 12
 
+f1 = fractions.Fraction(2, 10)
+f2 = fractions.Fraction(2, 12)
+
+print(f1, f2, f1 * f2, sep=",")
