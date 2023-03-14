a79dfe5a06100b2db12a758b054739ee0195cf2f
dmitrii dobrin <zatoichi.cadzicu@gmail.com>
2023-03-14 12:06:38 +0300

    path

--- HW/Lesson_005/filePath.py
+++ HW/Lesson_005/filePath.py
@@ -1,10 +1,13 @@
 # Напишите функцию, которая принимает на вход строку - абсолютный путь до файла.
 # Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+import os
 
-path = "G:\Python\HW_5.txt"
 
-def filePath():
-    stroke = path.split("\\")
-    return stroke
+def absolutePath():
+    fullName = os.path.basename(r'D:\TEMP\Advansed_Python\HW\Lesson_005\HW_5.txt')
+    fileName = os.path.splitext(fullName)[0]
+    fileExtention = os.path.splitext(fullName)[1]
+    pathFile = os.getcwd()
+    return pathFile, fileName, fileExtention
 
-print(filePath())
+print(absolutePath())
