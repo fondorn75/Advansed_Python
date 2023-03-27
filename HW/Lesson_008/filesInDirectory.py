453e2515508610f238810f795b70151f6623db5d
dmitrii dobrin <zatoichi.cadzicu@gmail.com>
2023-03-27 13:20:39 +0300

    json

--- HW/Lesson_008/filesInDirectory.py
+++ HW/Lesson_008/filesInDirectory.py
@@ -1,16 +1,34 @@
 import os
+import json
+import csv
+import pickle
 
-userPath = 'D:\\TEMP\\Advansed_Python\\HW\Lesson_007\\'
+userPath = 'D:\\TEMP\\Advansed_Python\\HW\\Lesson_008\\'
 
 
-def fileinDirectories(path, level=1):
-    print('Level=', level, 'Content:', os.listdir(path))
-    for i in os.listdir(path):
-        if os.path.isdir(path + '\\' + i):
-            print('Спускаемся', path + '\\' + i)
-            fileinDirectories(path + '\\' + i, level + 1)
-            print('Возвращаемся в', path)
+def fileinDirectories(path) -> dict:
+    d = {'name': os.path.basename(path)}
+    if os.path.isdir(path):
+        d['type'] = "directory"
+        d['children'] = [fileinDirectories(os.path.join(path, x)) for x in os.listdir(path)]
+    else:
+        d['type'] = "file"
+    return d
 
 
 if __name__ == "__main__":
-    fileinDirectories(userPath)
+    print(fileinDirectories(userPath))
+    with open('new_dir.json', 'w') as f:
+        json.dump(fileinDirectories(userPath), f)
+
+    temp = fileinDirectories(userPath)
+    # csvColumns = ['name', 'type', 'children']
+    # with open("dir.csv", 'w') as csvfile:
+    #     writer = csv.DictWriter(csvfile, fieldnames=csvColumns)
+    #     writer.writeheader()
+    #     for data in temp:
+    #         writer.writerow(data)
+
+    with open('myDir.pickle', 'wb') as f:
+        pickle.dump(temp, f)
+
